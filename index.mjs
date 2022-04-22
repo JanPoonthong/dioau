c487990f87d27e8f4b66896784cd273944ded25e
Jan Poonthong <janpoonthong628@gmail.com>
2022-04-22 23:52:50 +0700

    Separate the work into different function

--- index.mjs
+++ index.mjs
@@ -1,14 +1,32 @@
 import fetch from "node-fetch";
 import fs from "fs";
 
-async function getAllBook() {
+const filename = "book.json";
+
+function writeToFile(book) {
+  fs.writeFile(filename, JSON.stringify(book), (err) => {
+    if (err) return console.log(err);
+    console.log(`File created ${filename}`);
+  });
+}
+
+async function fetchingDataFromWebsite() {
   const response = await fetch('https://librivox.org/api/feed/audiobooks?extended=1&format=json');
+  console.log("Fethcing the data from website");
   return await response.json();
 }
 
-getAllBook().then((book) => {
-  fs.writeFile("book.json", JSON.stringify(book), (err) => {
-    if (err) return console.log(err);
-    console.log("Successful");
-  });
-})
+function checkFileExist() {
+  if (!fs.existsSync(filename)) {
+    const book = fetchingDataFromWebsite();
+    writeToFile(book);
+  } else {
+    console.log(`File exist ${filename}`);
+  }
+}
+
+async function getAllBook() {
+  checkFileExist()
+}
+
+getAllBook();
