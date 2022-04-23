3f8e1ec03bae2dff54bd19c807e97a646f080b0e
Jan Poonthong <janpoonthong628@gmail.com>
2022-04-23 16:52:30 +0700

    Implement limit and offset

--- index.mjs
+++ index.mjs
@@ -1,40 +1,51 @@
+import fsPromises from "fs/promises";
 import fetch from "node-fetch";
 import fs from "fs";
 
 const filename = "book.json";
 
-function writeToFile(book) {
-  fs.writeFile(filename, JSON.stringify(book), (err) => {
-    if (err) return console.log(err);
-    console.log(`File created ${filename}`);
-  });
+var offset = 0;
+var limit = 50;
+
+async function writeToFile(book) {
+  console.log(`Writing to ${filename}`);
+  await fsPromises.writeFile(filename, JSON.stringify(book), "utf-8");
 }
 
 async function fetchingDataFromWebsite() {
   const response = await fetch(
-    "https://librivox.org/api/feed/audiobooks?extended=1&format=json"
+    `https://librivox.org/api/feed/audiobooks?format=json&offset=${offset}&limit=${limit}`
   ).catch((err) => {
     return console.log(err);
   });
-  console.log("Fethcing the data from website");
+  console.log("Fetching the data from website");
   return await response.json();
 }
 
 async function checkFileExist() {
   if (!fs.existsSync(filename)) {
     const book = await fetchingDataFromWebsite();
-    writeToFile(book);
+    while (book.error !== "Audiobooks could not be found") {
+      const book = await fetchingDataFromWebsite();
+      offset += 50;
+      limit += 50;
+      await writeToFile(book);
+    }
   } else {
     console.log(`File exist ${filename}`);
   }
   return true;
 }
 
-function getAllBook() {
-  if (checkFileExist()) {
+async function getAllBook() {
+  if (await checkFileExist()) {
     fs.readFile("book.json", "utf8", function (err, data) {
       if (err) return console.log(err);
-      console.log(data);
+      const dataFromFileJSON = JSON.parse(data);
+      console.log(dataFromFileJSON);
+      // for (let i = 0; i < dataFromFileJSON.books.length; i++) {
+      //   console.log(dataFromFileJSON.books[i].title);
+      // }
     });
   }
 }
